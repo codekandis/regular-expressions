c8b9d73732239542f90b5d663f762ce042053aba
Christian Ramelow <info@codekandis.net>
2021-10-24 21:05:27 +0200

    Adds the new method `RegularExpressionInterface::replace()`.

--- src/RegularExpressionInterface.php
+++ src/RegularExpressionInterface.php
@@ -23,10 +23,22 @@ interface RegularExpressionInterface
 	 * Searches for all matches in a subject.
 	 * @param string $subject The subject to match the regular expression with.
 	 * @param bool $throwNoMatchException True if an exception must be thrown if the regular expression does not match, otherwise false.
-	 * @param int $flags The options of the search
+	 * @param int $flags The options of the search.
 	 * @param int $offset The offset to start the search from.
 	 * @return ?array The list of matches if found, otherwise null.
 	 * @throws RegularExpressionNotMatchingException The regular expression does not match.
 	 */
 	public function matchAll( string $subject, bool $throwNoMatchException, int $flags = 0, int $offset = 0 ): ?array;
+
+	/**
+	 * Replaces the matches in a subject.
+	 * @param string $replacement The replacement to replace the match with.
+	 * @param string $subject The subject to replace its matches.
+	 * @param bool $throwNoMatchException True if an exception must be thrown if the regular expression does not match, otherwise false.
+	 * @param int $limit The maximum possible replacements, otherwise -1.
+	 * @param ?int $count If specified, stores the amount of replacements done.
+	 * @return string The replaced string.
+	 * @throws RegularExpressionNotMatchingException The regular expression does not match.
+	 */
+	public function replace( string $replacement, string $subject, bool $throwNoMatchException, int $limit = -1, ?int &$count = null ): ?string;
 }
