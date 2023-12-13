e69bd8bdfe70ce43c5bf6eb5df6b918c2dffc54f
Christian Ramelow <info@codekandis.net>
2023-12-13 19:21:34 +0100

    Refactors the exception handling.

---
+++ src/InvalidLimitExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\RegularExpressions;
+
+/**
+ * Represents the interface of any exception if a limit is invalid.
+ * @package codekandis/regular-expressions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface InvalidLimitExceptionInterface
+{
+}
