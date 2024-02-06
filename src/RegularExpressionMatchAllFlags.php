f7132809af297e50b5d73b02070876c0d7794081
Christian Ramelow <info@codekandis.net>
2024-02-06 18:01:13 +0100

    Adds missing type hints.

--- src/RegularExpressionMatchAllFlags.php
+++ src/RegularExpressionMatchAllFlags.php
@@ -13,26 +13,26 @@ abstract class RegularExpressionMatchAllFlags
 	 * @see https://www.php.net/manual/en/function.preg-match.php
 	 * @var int
 	 */
-	public const OFFSET_CAPTURE = PREG_OFFSET_CAPTURE;
+	public const int OFFSET_CAPTURE = PREG_OFFSET_CAPTURE;
 
 	/**
 	 * Every unmatched subpattern will be returned as null.
 	 * @see https://www.php.net/manual/en/function.preg-match.php
 	 * @var int
 	 */
-	public const UNMATCHED_AS_NULL = PREG_UNMATCHED_AS_NULL;
+	public const int UNMATCHED_AS_NULL = PREG_UNMATCHED_AS_NULL;
 
 	/**
 	 * Orders the matches array in search pattern order.
 	 * @see https://www.php.net/manual/en/function.preg-match.php
 	 * @var int
 	 */
-	public const PATTERN_ORDER     = PREG_PATTERN_ORDER;
+	public const int PATTERN_ORDER = PREG_PATTERN_ORDER;
 
 	/**
 	 * Orders the array in sets of pattern matches.
 	 * @see https://www.php.net/manual/en/function.preg-match.php
 	 * @var int
 	 */
-	public const SET_ORDER         = PREG_SET_ORDER;
+	public const int SET_ORDER = PREG_SET_ORDER;
 }
